2377afe52997e17a0bde69367432fa2eb7013be4
Archadious <archadious@gmail.com>
2020-10-15 09:30:11 -0700

    Minor changes to output text

--- main.cc
+++ main.cc
@@ -11,26 +11,26 @@ using namespace std;
 //-----------------------------------------------------------------------------
 int main( int, char*[] ) {
 
-  mad::ring ring( { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } } );
-  cout << "ring" << endl << ring << endl << endl;
+  mad::ring one( { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } } );
+  cout << "one" << endl << one << endl << endl;
 
-  cout << "Test copy constructor" << endl;
-  mad::ring circle( ring );
+  cout << "Test copy constructor - 'mad::ring circle( one );'" << endl;
+  mad::ring circle( one );
   cout << endl << "circle" << endl << circle << endl << endl;
 
-  cout << "Test assignment operator" << endl;
+  cout << "Test assignment operator - 'square = circle;'" << endl;
   mad::ring square;
-  cout << endl << "square before: " << endl;
+  cout << "square before: " << endl;
   cout << square << endl << endl;
   square = circle;
   cout << endl << "square after: " << endl << square << endl << endl;
 
-  cout << "Test move constructor" << endl;
+  cout << "Test move constructor - 'mad::ring triangle( move( square ) );'" << endl;
   mad::ring triangle( move( square ) );
   cout << "triangle: " << endl << triangle << endl << endl;
   cout << endl << "square after move: " << endl << square << endl << endl;
 
-  cout << "Test move operator" << endl;
+  cout << "Test move operator - 'blob = move( triangle );'" << endl;
   mad::ring blob;
   cout << "blob before: " << endl << blob << endl << endl;
   blob = move( triangle );
